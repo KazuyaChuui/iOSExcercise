ec1d1bee78cfe7facb9d73542ab6ba36332af134
JesusRuiz <ruizluna.jesussalvador@gmail.com>
2021-07-30 15:40:05 -0600

    Initial Commit

---
+++ iOSExcercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSExcercise
+//
+//  Created by Jesus Ruiz on 7/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
